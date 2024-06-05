03f0d6d1b724bcb3e43345dfd7531cdbf05d33ca
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-06-04 23:39:38 -0300

    04/06
    parametros e rotas dinamicas

--- express e rotas/app.js
+++ express e rotas/app.js
@@ -5,18 +5,31 @@ let porta = 3000
 
 // como criar rotas?
 
-app.get("/", function(req, res){
-    res.send('Olá Mundo!');
-}) // rota criada para inserir dados na propria pagina
+app.get("/", function (req, res) {
+    res.send('Olá Mundo!')// rota criada para inserir dados na propria pagina
+})
 
 
-app.get("/sobre", function(req, res){
-    res.send('Aba sobre');
+app.get("/sobre", function (req, res) {
+    res.send('<h1> Aba sobre </h1>');
 }) // rota criada para outro tipos de resultados, no caso outra pagina
 
 
-app.listen(porta, function(){
-console.log(`Servidor ON, rodando em http://localhost:${porta}/`)
+
+
+//CRIAÇÃO DE PARAMETROS, ao colocar 2 pontos em um item apos o seu endereço, ele vira um parametro:
+app.get("/sobre/:nome/:sobrenome/:idade", function (req, res) { //esse so ira funcionar se for passado algo apos o teste/, é possivel passar parametros HTML mais para frente
+    res.send("<h1> Seja bem vindo </h1>" + req.params.nome + req.params.sobrenome + "<br>" + "<h2> Sua idade é </h2>" + req.params.idade); //atraves do req é possivel obter dados da requisao, afinal esse é o significado dela, como se fosse uma tabela JSON
+     
+    var teste = req.params
+    console.table(teste)
+})
+
+
+
+// funcionando o servidor >>
+app.listen(porta, function () {
+    console.log(`Servidor ON, rodando em http://localhost:${porta}/`)
 }); // função listen é importada do express, nela só é necessario passar o numero que vai ser atrelado na porta, mais simples do que criar um servidor de forma convencional
 // essa função dispara um evento, ela fica escutando algo acontecer na porta inserida
 
--- express e rotas/desc.md
+++ express e rotas/desc.md
@@ -3,3 +3,8 @@ Express.js é um framework para Node.js que fornece recursos mínimos para const
 criando servidor com express, de forma mais facil
 
 criando rotas e inserindo dados em paginas diferentes, descobrindo que o express é um framework baseado em rotas, para inserir conteudo
+
+
+parametros, são valores fornecidos após chamar uma função, o parametro só irá funcionar se na sua montagem houver : e quando for escrita ter algo escrito apor o endereço "localhost:3000/teste/parametro
+
+.send so pode ser utilizado uma vez
