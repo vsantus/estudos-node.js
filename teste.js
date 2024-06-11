923d5d9dd7f6506263ae079c399d19bebd505a02
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-06-10 23:39:34 -0300

    10/06
    ops, handlebars

--- teste.js
+++ teste.js
@@ -1,54 +1,54 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('teste', 'root', 'v$4N70$', { // banco de dados, usuario e senha para se conectaR
-    host: "localhost",
-    dialect: 'mysql'
-})
-
-// ver se está funcionando 
-
-// sequelize.authenticate().then(function(){ // .autenticate, função que me mostra se eu me conectei com o BD
-//     console.log("Conectado com Sucesso!!!");
-// }).catch(function(erro){
-//     console.log("Falha na conecção...")
-// }) 
-
-
-//criando models, criar uma tabela pelo sequelize 
-
-// const postagem = sequelize.define('postagens', {
-//     titulo: {
-//         type: Sequelize.STRING 
-//     },
-//     conteudo: {
-//         type: Sequelize.STRING
-//     },
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('teste', 'root', 'password', { // banco de dados, usuario e senha para se conectaR
+//     host: "localhost",
+//     dialect: 'mysql'
 // })
 
-//como rodar model no sql
+// // ver se está funcionando 
 
-// postagem.sync({force:true}) // .sync sincroniza o codigo com o sql, e o force me deixa seguro que a tabela foi criada
+// // sequelize.authenticate().then(function(){ // .autenticate, função que me mostra se eu me conectei com o BD
+// //     console.log("Conectado com Sucesso!!!");
+// // }).catch(function(erro){
+// //     console.log("Falha na conecção...")
+// // }) 
 
-const usuario = sequelize.define('usuarios', {
-    nome:{
-        type: Sequelize.STRING
-    },
-    sobrenome:{
-        type: Sequelize. STRING
-    },
-    idade: {
-        type: Sequelize.INTEGER //INT
-    },
-    email: {
-        type: Sequelize.STRING
-    }
-})
 
-usuarios.create({
-    nome: "Vsantos",
-    sobrenome: "DeFato",
-    idade: 20,
-    email: "teste@gmail.com"
-})
+// //criando models, criar uma tabela pelo sequelize 
 
-usuario.sync({force:true}) 
+// // const postagem = sequelize.define('postagens', {
+// //     titulo: {
+// //         type: Sequelize.STRING 
+// //     },
+// //     conteudo: {
+// //         type: Sequelize.STRING
+// //     },
+// // })
+
+// //como rodar model no sql
+
+// // postagem.sync({force:true}) // .sync sincroniza o codigo com o sql, e o force me deixa seguro que a tabela foi criada
+
+// const usuario = sequelize.define('usuarios', {
+//     nome:{
+//         type: Sequelize.STRING
+//     },
+//     sobrenome:{
+//         type: Sequelize. STRING
+//     },
+//     idade: {
+//         type: Sequelize.INTEGER //INT
+//     },
+//     email: {
+//         type: Sequelize.STRING
+//     }
+// })
+
+// usuarios.create({
+//     nome: "Vsantos",
+//     sobrenome: "DeFato",
+//     idade: 20,
+//     email: "teste@gmail.com"
+// })
+
+// usuario.sync({force:true}) 
 
